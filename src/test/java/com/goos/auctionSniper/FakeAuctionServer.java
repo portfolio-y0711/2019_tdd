36dfb85fc13746dfa29a84dc33bd15ae832d5bb5
yoonsung0711 <yoonsung0711@gmail.com>
2019-01-18 18:46:22 +0900

    p112(1). XMPP 프로토콜 기반의 openfire 서버와 통신을 담당하는 가짜 경매 서버를 작성하여 사우스비 경매 서버와의 교신을 대체함

---
+++ src/test/java/com/goos/auctionSniper/FakeAuctionServer.java
@@ -0,0 +1,22 @@
+package com.goos.auctionSniper;
+
+public class FakeAuctionServer {
+    public FakeAuctionServer(String s) {
+    }
+
+    public Object getItemId() {
+        return "auction-item-54321";
+    }
+
+    public void startSellingItem() {
+    }
+
+    public void hasReceivedJoinRequestFromSniper() {
+    }
+
+    public void announceClosed() {
+    }
+
+    public void stop() {
+    }
+}
