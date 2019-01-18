7f668917c4b4c888f41f9e68ec3a9437903415a1
yoonsung0711 <yoonsung0711@gmail.com>
2019-01-18 20:40:22 +0900

    p113(2). openfire에 전달한 리스너는 메시지 발생시 이를 가짜 경매 서버의 큐에 등록하는데, 해당 큐에 추가된 메시지 존재 여부 확인을 위해 receivesAMessage 메서드 등을 추가로 구현함

--- src/test/java/com/goos/auctionSniper/FakeAuctionServer.java
+++ src/test/java/com/goos/auctionSniper/FakeAuctionServer.java
@@ -6,6 +6,7 @@ import org.jivesoftware.smack.packet.Message;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
@@ -40,13 +41,16 @@ public class FakeAuctionServer {
         return itemId;
     }
 
-    public void hasReceivedJoinRequestFromSniper() {
+    public void hasReceivedJoinRequestFromSniper() throws InterruptedException {
+        messageListner.receivesAMessage();
     }
 
-    public void announceClosed() {
+    public void announceClosed() throws XMPPException {
+        currentChat.sendMessage(new Message());
     }
 
     public void stop() {
+        connection.disconnect();
     }
 
     public class SingleMessageListener implements MessageListener {
@@ -55,6 +59,10 @@ public class FakeAuctionServer {
         @Override
         public void processMessage(Chat chat, Message message) {
             messages.add(message);
+        }
+
+        public void receivesAMessage() throws InterruptedException {
+            assertThat("Message", messages.poll(5, SECONDS), is(notNullValue()));
         }
     }
 }
