9b423336dcf3887de3c045b3aaf9c10bfac3e906
yoonsung0711 <yoonsung0711@gmail.com>
2019-01-18 18:50:27 +0900

    p112(2). 경매스나이퍼 프로그램의 진입점이 되는 Main 클래스를 작성

---
+++ src/main/java/com/goos/auctionSniper/Main.java
@@ -0,0 +1,8 @@
+package com.goos.auctionSniper;
+
+public class Main {
+    public static final String SNIPER_STATUS_LABEL = "Label-SniperStatus";
+
+    public static void main(String... args) {
+    }
+}
