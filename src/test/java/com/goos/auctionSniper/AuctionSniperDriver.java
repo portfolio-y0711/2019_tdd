d02bedef4dd9cd87d03be30e015639fc7ddf0dae
yoonsung0711 <yoonsung0711@gmail.com>
2019-01-18 18:39:26 +0900

    p110(1). GUI 컨트롤을 위해 윈도리커 드라이버를 컴포지션 방식으로 확장함

---
+++ src/test/java/com/goos/auctionSniper/AuctionSniperDriver.java
@@ -0,0 +1,12 @@
+package com.goos.auctionSniper;
+
+public class AuctionSniperDriver {
+    public AuctionSniperDriver(int timeoutMillis) {
+    }
+
+    public void showSniperStatus(String statusText) {
+    }
+
+    public void dispose() {
+    }
+}
