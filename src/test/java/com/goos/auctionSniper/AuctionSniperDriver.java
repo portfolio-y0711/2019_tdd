b3b9336794b6988fed2a5651d8df6ddae9150ca8
yoonsung0711 <yoonsung0711@gmail.com>
2019-01-18 19:26:09 +0900

    p110(2). 윈도리커 JFrameDriver 클래스 생성자를 이용, 경매스나이퍼의 Frame 및 라벨 드라이버를 생성함

--- src/test/java/com/goos/auctionSniper/AuctionSniperDriver.java
+++ src/test/java/com/goos/auctionSniper/AuctionSniperDriver.java
@@ -1,10 +1,23 @@
 package com.goos.auctionSniper;
 
-public class AuctionSniperDriver {
+import com.objogate.wl.swing.AWTEventQueueProber;
+import com.objogate.wl.swing.driver.JFrameDriver;
+import com.objogate.wl.swing.driver.JLabelDriver;
+import com.objogate.wl.swing.gesture.GesturePerformer;
+
+import static org.hamcrest.Matchers.equalTo;
+
+public class AuctionSniperDriver extends JFrameDriver {
     public AuctionSniperDriver(int timeoutMillis) {
+        super(new GesturePerformer(),
+                JFrameDriver.topLevelFrame(named("Auction Sniper Standalone"), showingOnScreen()),
+                new AWTEventQueueProber(timeoutMillis, 100));
+
     }
 
     public void showSniperStatus(String statusText) {
+        new JLabelDriver(this, named(Main.SNIPER_STATUS_LABEL))
+        .hasText(equalTo(statusText));
     }
 
     public void dispose() {
