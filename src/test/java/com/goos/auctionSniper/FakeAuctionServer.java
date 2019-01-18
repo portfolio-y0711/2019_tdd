1c29251ce4f0c315a2b853800e4dd934bc9a1f35
yoonsung0711 <yoonsung0711@gmail.com>
2019-01-18 20:00:44 +0900

    p112(3). openfire에 접속/로그인/'리스너 등록' 등을 수행하는 경매 준비 작업을 추상화 한 startSellingItem 메서드를 가짜 경매 서버에 구현

--- src/test/java/com/goos/auctionSniper/FakeAuctionServer.java
+++ src/test/java/com/goos/auctionSniper/FakeAuctionServer.java
@@ -1,17 +1,38 @@
 package com.goos.auctionSniper;
 
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.ChatManagerListener;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+
 public class FakeAuctionServer {
-    public FakeAuctionServer(String s) {
+    private final String itemId;
+    private final XMPPConnection connection;
+    private Chat currentChat;
+
+    public FakeAuctionServer(String itemId) {
+        this.itemId = itemId;
+        this.connection = new XMPPConnection("localhost");
+    }
+
+    public void startSellingItem() throws XMPPException {
+        connection.connect();
+        connection.login("auction-item-54321", "auction", "Auction");
+        connection.getChatManager().addChatListener(
+                new ChatManagerListener() {
+                    @Override
+                    public void chatCreated(Chat chat, boolean createdLocally) {
+                        currentChat = chat;
+                    }
+                }
+        );
     }
 
     public Object getItemId() {
-        return "auction-item-54321";
+        return itemId;
     }
 
-    public void startSellingItem() {
-    }
-
-    public void hasReceivedJoinRequestFromSniper() {
+    public voiㅏd hasReceivedJoinRequestFromSniper() {
     }
 
     public void announceClosed() {
