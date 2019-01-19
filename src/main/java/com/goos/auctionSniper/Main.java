3d7423964331cc8f54672b98e5f6fcbe88d0209c
yoonsung0711 <yoonsung0711@gmail.com>
2019-01-19 20:43:33 +0900

    p115(1). 경매 스나이퍼 인터페이스를 구성하는 JFrame 컴포넌트 구현

--- src/main/java/com/goos/auctionSniper/Main.java
+++ src/main/java/com/goos/auctionSniper/Main.java
@@ -1,8 +1,22 @@
 package com.goos.auctionSniper;
 
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+import java.lang.reflect.InvocationTargetException;
+
 public class Main {
     public static final String SNIPER_STATUS_LABEL = "Label-SniperStatus";
 
-    public static void main(String... args) {
+    public static void main(String... args) throws InvocationTargetException, InterruptedException {
+        SwingUtilities.invokeAndWait(new Runnable() {
+            @Override
+            public void run() {
+                JFrame mainWindow = new JFrame("Auction Sniper");
+                mainWindow.setName("Auction Sniper Standalone");
+                mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                mainWindow.setVisible(true);
+            }
+        });
     }
 }
