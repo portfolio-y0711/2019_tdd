b7ecb925285754b3c3375e8a92db69de1db76f15
yoonsung0711 <yoonsung0711@gmail.com>
2019-01-19 21:37:06 +0900

    p115(3). 사용자 인터페이스 로딩 로직을 별도의 메소드로 분리하고 하드코딩된 부분을 멤버 변수로 대체함

--- src/main/java/com/goos/auctionSniper/Main.java
+++ src/main/java/com/goos/auctionSniper/Main.java
@@ -7,23 +7,37 @@ import java.lang.reflect.InvocationTargetException;
 
 public class Main {
     public static final String SNIPER_STATUS_LABEL = "Label-SniperStatus";
+    private static MainWindow ui;
+
+    public Main() throws InvocationTargetException, InterruptedException {
+        startUserInterface();
+    }
 
     public static void main(String... args) throws InvocationTargetException, InterruptedException {
+        Main main = new Main();
+    }
+
+    public static void startUserInterface() throws InterruptedException, InvocationTargetException {
         SwingUtilities.invokeAndWait(new Runnable() {
             @Override
             public void run() {
-                MainWindow ui = new MainWindow();
+                ui = new MainWindow();
             }
         });
     }
+
     public static class MainWindow extends JFrame {
+
+        private final String sniperStatus = "Sniper Status: Joined";
+        private final String SNIPER_STATUS_LABEL_NAME = "Label-SniperStatus";
+
         MainWindow(){
             JFrame mainWindow = new JFrame("Auction Sniper");
             mainWindow.setName("Auction Sniper Standalone");
             mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             mainWindow.setVisible(true);
 
-            JLabel statusLabel = createLabel("Sniper Status: Joined");
+            JLabel statusLabel = createLabel(sniperStatus);
 
             mainWindow.add(statusLabel);
             mainWindow.pack();
@@ -31,7 +45,7 @@ public class Main {
 
         private JLabel createLabel(String initialText) {
             JLabel statusLabel = new JLabel(initialText);
-            statusLabel.setName("Label-SniperStatus");
+            statusLabel.setName(SNIPER_STATUS_LABEL_NAME);
             statusLabel.setBorder(new LineBorder(Color.BLACK));
             return statusLabel;
         }
