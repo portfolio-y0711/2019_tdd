d69222359607b0297c0adb7a010e7501f9efc1a2
yoonsung0711 <yoonsung0711@gmail.com>
2019-01-20 19:14:34 +0900

    p126(1). 입찰 기능을 구현을 위해 E2E에 테스트 코드 추가

--- src/test/java/com/goos/auctionSniper/AuctionSniperEndToEndTest.java
+++ src/test/java/com/goos/auctionSniper/AuctionSniperEndToEndTest.java
@@ -14,11 +14,20 @@ public class AuctionSniperEndToEndTest {
         auction.startSellingItem();
         application.startBiddingIn(auction);
         auction.hasReceivedJoinRequestFromSniper();
+        auction.announceClosed();
+        application.showSniperHasLostAuction();
+    }
+
+    @Test
+    public void sniperMakesAHigherBidButLoses() throws XMPPException, InterruptedException {
+        auction.startSellingItem();
+        application.startBiddingIn(auction);
 
         auction.reportPrice(1000, 98, "other bidder");
         application.hasShownSniperIsBidding();
         auction.hasReceivedBid(1098, "sniper@sanalucet.duckdns.org/Auction");
 
+        auction.hasReceivedJoinRequestFromSniper();
         auction.announceClosed();
         application.showSniperHasLostAuction();
     }
